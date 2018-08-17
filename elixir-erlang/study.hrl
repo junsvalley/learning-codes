86080ffbd46fc63a91b034089243e5fb68931ca8
gufeng <gufeng2011.nt@gmail.com>
2018-08-17 14:28:04 +0800

    delete some files

--- elixir-erlang/study.hrl
+++
@@ -1,7 +0,0 @@
--record(records, { key1 = default1, key2 = deafult2 }).
-
--ifdef(debug).
--define(TRACE(X), io:format("[~s]<~s>:~B\t ~s~n", [?MODULE, ?FILE, ?LINE, X])).
--else.
--define(TRACE(X), void).
--endif.
--- python/h2.py
+++
@@ -1,6 +0,0 @@
-import urllib
-import webbrowser as web
-url = "http://www.163.com"
-content = urllib.urlopen(url).read()
-open("jeapedu.html", "w").write(content)
-web.open_new_tab('jeapedu.html')
